eda5e2799726b6d9eb4245f3c1b04332ebf68038
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-03 16:19:59 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+stack<int> s;
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> N;
+
+	while (N--)
+	{
+		string op;
+		cin >> op;
+
+		if (op == "push")
+		{
+			int data;
+			cin >> data;
+			s.push(data);
+		}
+		else if (op == "pop")
+		{
+			if (s.empty())
+				cout << -1 << '\n';
+			else
+			{
+				cout << s.top() << '\n';
+				s.pop();
+			}
+		}
+		else if (op == "size")
+		{
+			cout << s.size() << '\n';
+		}
+		else if (op == "empty")
+		{
+			cout << s.empty() << '\n';
+		}
+		else if (op == "top")
+		{
+			if (s.empty())
+				cout << -1 << '\n';
+			else
+				cout << s.top() << '\n';
+		}
+	}
+}
