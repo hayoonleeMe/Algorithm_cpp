d0371af811942b0daf9fbeabf7a771fa8389ab9c
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-03 15:58:39 +0900

    [Silver II] Title: 주식, Time: 648 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/11501. 주식/주식.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int T, N;
+int a[1'000'002];
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> T;
+	while (T--)
+	{
+		cin >> N;
+		for (int i = 0; i < N; ++i)
+			cin >> a[i];
+
+		long long ans = 0;
+
+		int mx = a[N - 1];
+		for (int i = N - 2; i >= 0; --i)
+		{
+			if (a[i] < mx)
+				ans += mx - a[i];
+			else
+				mx = a[i];
+		}
+		
+		cout << ans << '\n';
+	}
+}
