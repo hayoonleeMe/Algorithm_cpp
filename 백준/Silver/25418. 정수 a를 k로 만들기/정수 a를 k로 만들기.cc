e25399acbe521c6fc6233ad4f74fd0ee6feb7701
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-03 16:02:29 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 12 ms, Memory: 6588 KB -BaekjoonHub

---
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int A, K;
+int dist[1000005];
+queue<int> q;
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> A >> K;
+    fill(dist, dist + 1000005, -1);
+
+	dist[A] = 0;
+	q.push(A);
+
+	while (!q.empty())
+	{
+		int cur = q.front();
+		q.pop();
+
+		for (int nx : { cur + 1, cur * 2 })
+		{
+			if (nx > 1000005)
+				continue;
+			if (dist[nx] != -1)
+				continue;
+
+			dist[nx] = dist[cur] + 1;
+			q.push(nx);
+		}
+	}
+
+	cout << dist[K];
+}
