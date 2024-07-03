b8122e458d0f71b96de044ddd0c9316613ca1638
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-03 16:07:38 +0900

    [Silver I] Title: 오르막 수, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mod = 10007;
+int N;
+int d[1005][10];
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> N;
+
+	for (int i = 0; i <= 9; ++i)
+		d[1][i] = 1;
+
+	for (int i = 2; i <= N; ++i)
+	{
+		for (int j = 0; j <= 9; ++j)
+		{
+			int sum = 0;
+			for (int k = j; k <= 9; ++k)
+				sum = (sum + d[i - 1][k]) % mod;
+			d[i][j] = sum;
+
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i <= 9; ++i)
+		ans = (ans + d[N][i]) % mod;
+	cout << ans;
+}
