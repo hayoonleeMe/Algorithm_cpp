4d75338078705e43c1c73a94a7fd18f150c69ad2
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-03 16:14:37 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int seq[10];
+
+void func(int cur)
+{
+	if (cur == M)
+	{
+		for (int i = 0; i < M; ++i)
+			cout << seq[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= N; ++i)
+	{ 
+		if (cur > 0 && seq[cur - 1] > i)
+			continue;
+
+		seq[cur] = i;
+		func(cur + 1);
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> N >> M;
+
+	func(0);
+}
