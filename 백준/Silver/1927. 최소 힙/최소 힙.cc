1b6ebfd58bec888c1595258cdbb5a3e5df4e878a
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-03 15:46:30 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	int N;
+	cin >> N;
+	while (N--)
+	{
+		int x;
+		cin >> x;
+
+		if (x > 0)
+			pq.push(x);
+		else
+		{
+			if (!pq.empty())
+			{
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+			else
+				cout << 0 << '\n';
+		}
+	}
+}
