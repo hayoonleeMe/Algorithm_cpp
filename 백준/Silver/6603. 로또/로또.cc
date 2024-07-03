172ba1e50034114472346694c19baadb977c26d2
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-03 16:13:04 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int K;
+int arr[15];
+int mask[15];
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	for (cin >> K; K != 0; cin >> K)
+	{
+		for (int i = 0; i < K; ++i)
+			cin >> arr[i];
+		for (int i = 6; i < K; ++i)
+			mask[i] = 1;
+
+		do
+		{
+			for (int i = 0; i < K; ++i)
+				if (!mask[i])
+					cout << arr[i] << ' ';
+			cout << '\n';
+		} while (next_permutation(mask, mask + K));
+		cout << '\n';
+	}
+}
