b961e01795d3211da2d3f3a4397c0ff7e37f65d6
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-03 15:57:06 +0900

    [Silver IV] Title: 파스칼 삼각형, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Silver/15489. 파스칼 삼각형/파스칼 삼각형.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int R, C, W;
+long long d[52][52];
+long long ans;
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> R >> C >> W;
+
+	for (int r = 1; r < R + W; ++r)
+	{
+		for (int c = 1; c <= r; ++c)
+		{
+			if (c == 1 || c == r)
+				d[r][c] = 1;
+			else
+				d[r][c] = d[r - 1][c - 1] + d[r - 1][c];
+		}
+	}
+
+	int cL = C;
+	for (int r = R; r < R + W; ++r)
+	{
+		for (int c = C; c <= cL; ++c)
+			ans += d[r][c];
+		++cL;
+	}
+
+	cout << ans;
+}
