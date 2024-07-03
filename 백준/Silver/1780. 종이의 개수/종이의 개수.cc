58a3e0bf0fc21965073e1a2ddff1734071e9fba9
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-03 16:15:10 +0900

    [Silver II] Title: 종이의 개수, Time: 400 ms, Memory: 20928 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int board[2200][2200];
+int ans[3];
+
+void func(int n, int r1, int c1)
+{
+	if (n == 0)
+		return;
+
+	// 숫자가 같은지 확인
+	int t = board[r1][c1];
+	for (int i = r1; i < r1 + n; ++i)
+	{
+		for (int j = c1; j < c1 + n; ++j)
+		{
+			if (t != board[i][j])
+			{
+				int off = n / 3;
+				func(off, r1, c1);
+				func(off, r1, c1 + off);
+				func(off, r1, c1 + 2 * off);
+
+				func(off, r1 + off, c1);
+				func(off, r1 + off, c1 + off);
+				func(off, r1 + off, c1 + 2 * off);
+
+				func(off, r1 + 2 * off, c1);
+				func(off, r1 + 2 * off, c1 + off);
+				func(off, r1 + 2 * off, c1 + 2 * off);
+
+				return;
+			}
+		}
+	}
+
+	if (t == -1)
+		++ans[0];
+	else if (t == 0)
+		++ans[1];
+	else if (t == 1)
+		++ans[2];
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < N; ++j)
+			cin >> board[i][j];
+
+	func(N, 0, 0);
+
+	cout << ans[0] << '\n' << ans[1] << '\n' << ans[2];
+}
