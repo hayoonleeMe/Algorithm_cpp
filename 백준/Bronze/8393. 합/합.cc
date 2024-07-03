8daeabf04efb35447dbf655b407f6b53ad74c1fa
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-03 16:03:28 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	int ans = 0;
+	for (int i = 1; i <= n; ++i)
+		ans += i;
+	cout << ans;
+}
