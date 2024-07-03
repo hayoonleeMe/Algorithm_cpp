cdab078fc4d2c0151ba2134080db366cf79df492
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-03 16:20:06 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 96 ms, Memory: 2284 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, K;
+list<int> seq;
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> N >> K;
+	for (int i = 1; i <= N; ++i)
+		seq.push_back(i);
+
+	list<int>::iterator c = seq.begin();
+	cout << "<";
+	while (seq.size() > 1)
+	{
+		for (int j = 0; j < K - 1; ++j)
+		{
+			++c;
+			if (c == seq.end())
+				c = seq.begin();
+		}
+
+		cout << *c << ", ";
+		c = seq.erase(c);
+		if (c == seq.end())
+			c = seq.begin();
+	}
+	cout << *c << ">";
+}
