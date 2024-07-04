49241708ee0184ded87ac3a46da0cc0404695433
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-04 15:09:10 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -11,17 +11,14 @@ int main()
 
 	cin >> N;
 	for (int i = 0; i < N; ++i)
-	{
 		cin >> arr[i];
-		d[i] = arr[i];
-	}
 
-	for (int i = 1; i < N; ++i)
+	for (int i = 0; i <= N; ++i)
 	{
+		d[i] = arr[i];
 		for (int j = i - 1; j >= 0; --j)
 			if (arr[i] > arr[j])
 				d[i] = max(d[i], d[j] + arr[i]);
 	}
-
-	cout << *max_element(d, d + N);
+	std::cout << *max_element(d, d + N);
 }
