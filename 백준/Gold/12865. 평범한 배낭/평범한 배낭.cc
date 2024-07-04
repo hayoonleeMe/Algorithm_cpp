e263af6a98b9b8efe3d69ed8706e8a876960427a
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-04 17:17:34 +0900

    [Gold V] Title: 평범한 배낭, Time: 36 ms, Memory: 43040 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, K, w, v;
-// W, V
-int W[102], V[102];
-// d[i] : 물품의 개수가 i개일 때 최대 가치합
-int d[102][100'002];
+int N, K;
+int W[105];
+int V[105];
+int d[105][100005];
 
 int main()
 {
@@ -13,24 +12,17 @@ int main()
 
 	cin >> N >> K;
 	for (int i = 1; i <= N; ++i)
-	{
-		cin >> w >> v;
-		W[i] = w;
-		V[i] = v;
-	}
+		cin >> W[i] >> V[i];
 
-	for (int k = 1; k <= K; ++k)
+	for (int i = 1; i <= N; ++i)
 	{
-		for (int i = 1; i <= N; ++i)
-		{
-			// i번째 물건을 배낭에 못넣을때
-			if (W[i] > k)
-				d[i][k] = d[i - 1][k];
-			else
-				// i번째 물건 안넣기 vs 넣기
+		for (int k = 1; k <= K; ++k)
+		{ 
+			if (k - W[i] >= 0)
 				d[i][k] = max(d[i - 1][k], V[i] + d[i - 1][k - W[i]]);
+			else
+				d[i][k] = d[i - 1][k];
 		}
 	}
-
 	cout << d[N][K];
 }
