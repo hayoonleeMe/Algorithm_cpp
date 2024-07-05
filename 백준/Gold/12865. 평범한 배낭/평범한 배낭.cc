08667f1e8046ae667a0936a4aa325644492d9e8a
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-05 17:07:05 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -4,25 +4,23 @@ using namespace std;
 int N, K;
 int W[105];
 int V[105];
-int d[105][100005];
+int d[100005];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N >> K;
-	for (int i = 1; i <= N; ++i)
+	for (int i = 0; i < N; ++i)
 		cin >> W[i] >> V[i];
 
-	for (int i = 1; i <= N; ++i)
+	for (int i = 0; i < N; ++i)
 	{
-		for (int k = 1; k <= K; ++k)
-		{ 
-			if (k - W[i] >= 0)
-				d[i][k] = max(d[i - 1][k], V[i] + d[i - 1][k - W[i]]);
-			else
-				d[i][k] = d[i - 1][k];
+		for (int j = K; j > 0; --j)
+		{
+			if (j >= W[i])
+				d[j] = max(d[j], d[j - W[i]] + V[i]);
 		}
 	}
-	cout << d[N][K];
+	cout << *max_element(d, d + K + 1);
 }
