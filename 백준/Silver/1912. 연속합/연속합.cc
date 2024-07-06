762ae85601ba0e14244b6092cf1c67e50ec144a4
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-06 15:35:26 +0900

    [Silver II] Title: 연속합, Time: 4 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int arr[100005];
-int d[100005];
+int n;
+int arr[100'005];
+int d[100'005][2];
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	cin >> N;
-	for (int i = 1; i <= N; ++i)
-	{
+	cin >> n;
+	for (int i = 0; i < n; ++i)
 		cin >> arr[i];
-		d[i] = arr[i];
+
+	d[0][0] = -1005;
+	d[0][1] = arr[0];
+	for (int i = 1; i < n; ++i)
+	{
+		d[i][0] = max(d[i - 1][0], d[i - 1][1]);
+		d[i][1] = max(arr[i], d[i - 1][1] + arr[i]);
 	}
-
-	for (int i = 1; i <= N; ++i)
-		d[i] = max(d[i], d[i - 1] + arr[i]);
-
-	cout << *max_element(d + 1, d + N + 1);
+	cout << max(d[n - 1][0], d[n - 1][1]);
 }
