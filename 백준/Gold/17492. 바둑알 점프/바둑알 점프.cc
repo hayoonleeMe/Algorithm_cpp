64d0dfe723dfd747588fe9cd3458720453ceac0b
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-07-30 20:57:25 +0900

    [Gold IV] Title: 바둑알 점프, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17492. 바둑알 점프/바둑알 점프.cc
+++ 백준/Gold/17492. 바둑알 점프/바둑알 점프.cc
@@ -9,7 +9,7 @@ vector<pair<int, int>> v;
 
 bool ans;
 
-pair<int, int> dirs[8] = { {0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, { -1,-1}, {-1,0}, {-1,1} };
+pair<int, int> dirs[9] = { {0,0}, {0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}, {-1,0}, {-1,1} };
 
 void Solve(int k)
 {
@@ -19,21 +19,26 @@ void Solve(int k)
 		return;
 	}
 
-	for (auto cur : v)
+	for (int i = 0; i < v.size(); ++i)
 	{
+		auto cur = v[i];
+
+		if (board[cur.X][cur.Y] != 2)
+			continue;
+
 		for (auto dir : dirs)
 		{
 			int nx = cur.X + dir.X;
 			int ny = cur.Y + dir.Y;
 
-			if (nx <= 0 || nx > N || ny <= 0 || ny > N)
+			if (nx < 0 || nx >= N || ny < 0 || ny >= N)
 				continue;
 			if (board[nx][ny] != 2)
 				continue;
 
 			int nnx = nx + dir.X;
 			int nny = ny + dir.Y;
-			if (nnx <= 0 || nnx > N || nny <= 0 || nny > N)
+			if (nnx < 0 || nnx >= N || nny < 0 || nny >= N)
 				continue;
 			if (board[nnx][nny] != 0)
 				continue;
@@ -41,12 +46,17 @@ void Solve(int k)
 			board[nx][ny] = 0;
 			board[cur.X][cur.Y] = 0;
 			board[nnx][nny] = 2;
+			v[i] = { nnx, nny };
 
 			Solve(k - 1);
 
 			board[nx][ny] = 2;
 			board[cur.X][cur.Y] = 2;
 			board[nnx][nny] = 0;
+			v[i] = cur;
+
+			if (ans)
+				return;
 		}
 	}
 }
@@ -56,8 +66,8 @@ int main()
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
 	cin >> N;
-	for (int i = 1; i <= N; ++i)
-		for (int j = 1; j <= N; ++j)
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < N; ++j)
 		{
 			cin >> board[i][j];
 			if (board[i][j] == 2)
