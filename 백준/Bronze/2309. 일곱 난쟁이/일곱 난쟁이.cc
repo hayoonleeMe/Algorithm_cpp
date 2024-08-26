c6c1c5b0513a1cf4083ab509aa21d0b73907fd32
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-08-26 18:10:15 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	int arr[9], sum = 0;
+	for (int i = 0; i < 9; ++i)
+	{
+		cin >> arr[i];
+		sum += arr[i];
+	}
+	sort(arr, arr + 9);
+
+	int i, j;
+	for (i = 0; i < 9; ++i)
+	{
+		for (j = i + 1; j < 9; ++j)
+		{
+			if (sum - arr[i] - arr[j] == 100)
+			{
+				for (int k = 0; k < 9; ++k)
+					if (k != i && k != j)
+						cout << arr[k] << '\n';
+				return 0;
+			}
+		}
+	}
+}
