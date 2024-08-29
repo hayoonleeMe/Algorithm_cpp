641508efedd8630452edd225f1229c88fa1a6c83
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-08-29 21:00:36 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,25 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-stack<char> sta;
+string s, temp;
 
 int main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	string s;
 	cin >> s;
-
-	bool even = s.size() % 2 == 1;
-	for (int i = 0; i < s.size(); ++i)
-	{
-		if (even && i == s.size() / 2)
-			continue;
-
-		if (!sta.empty() && sta.top() == s[i])
-			sta.pop();
-		else
-			sta.push(s[i]);
-	}
-	cout << sta.empty();
+	temp = s;
+	reverse(temp.begin(), temp.end());
+	cout << (temp == s);
 }
