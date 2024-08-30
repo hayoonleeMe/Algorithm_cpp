d321d54afe36c9ab3a36343f3cb6106ab9042e6c
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-08-30 17:17:12 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+typedef long long ll;
 ll A, B, C;
 
-ll POW(ll a, ll b, ll m)
-{
-	if (b == 1)
-		return a % m;
-	ll val = POW(a, b / 2, m);
-	val = val * val % m;
-	if (b % 2 == 0)
-		return val;
-	return val * a % m;
+ll go(ll a, ll b) {
+	if (b == 1) return a % C;
+
+	ll ret = go(a, b / 2);
+	ret = (ret * ret) % C;
+	if (b % 2) ret = (ret * a) % C;
+	return ret;
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(0); cin.tie(0);
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 	cin >> A >> B >> C;
-	cout << POW(A, B, C);
+	cout << go(A, B);
 }
