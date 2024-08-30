f80faadaba24e7a9e07637bd9540fd9d33627291
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-08-30 16:44:53 +0900

    [Silver IV] Title: 좋은 단어, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -1,36 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, ans;
+int n, ret;
+string s;
 
 int main()
 {
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	
-	cin >> N;
-	while (N--)
-	{
-		stack<char> s;
-		string str;
-		cin >> str;
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-		for (char c : str)
-		{
-			if (s.empty())
-			{
-				s.push(c);
-				continue;
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> s;
+
+		stack<char> st;
+		for (char c : s) {
+			if (st.empty()) st.push(c);
+			else {
+				if (c == st.top()) st.pop();
+				else st.push(c);
 			}
-
-			if (s.top() == c)
-				s.pop();
-			else
-				s.push(c);
 		}
-
-		if (s.empty())
-			++ans;
+		if (st.empty()) ++ret;
 	}
-
-	cout << ans;
+	cout << ret;
 }
