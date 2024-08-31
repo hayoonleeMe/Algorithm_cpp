7714276ae673b679d7512bd5fa205351bf7807d8
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-08-31 23:19:56 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2108 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -3,15 +3,13 @@ using namespace std;
 
 const int dy[] = { -1, 0, 1, 0 };
 const int dx[] = { 0, 1, 0, -1 };
-int n, m, y, x, visited[104][104];
-string board[104];
+int n, m, y, x, board[104][104], visited[104][104];
 
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-	cin >> n >> m;
+	scanf("%d %d", &n, &m);
 	for (int i = 0; i < n; ++i)
-		cin >> board[i];
+		for (int j = 0; j < m; ++j)
+			scanf("%1d", &board[i][j]);
 
 	queue<pair<int, int>> q;
 	visited[0][0] = 1;
@@ -25,11 +23,11 @@ int main() {
 			int ny = y + dy[dir];
 			int nx = x + dx[dir];
 			if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-			if (board[ny][nx] == '0' || visited[ny][nx]) continue;
+			if (board[ny][nx] == 0 || visited[ny][nx]) continue;
 
 			visited[ny][nx] = visited[y][x] + 1;
 			q.push({ ny, nx });
 		}
 	}
-	cout << visited[n - 1][m - 1];
+	printf("%d", visited[n - 1][m - 1]);
 }
