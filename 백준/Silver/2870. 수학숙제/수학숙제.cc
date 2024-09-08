cb46a8981a90a57cb5a65358377da3ec3c2f70f2
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-09-08 15:58:43 +0900

    [Silver IV] Title: 수학숙제, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Silver/2870. 수학숙제/수학숙제.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, cnt, st;
+string s;
+vector<string> ret;
+
+string trim(string a) {
+	int st = 0;
+	for (int i = 0; i < a.size() - 1; ++i) {
+		if (a[i] == '0') ++st;
+		else break;
+	}
+	return a.substr(st);
+}
+
+bool compare(const string& a, const string& b) {
+	if (a.size() == b.size()) {
+		int i = 0;
+		for (; i < a.size() - 1 && a[i] == b[i]; ++i);
+		return a[i] - '0' < b[i] - '0';
+	}
+	return a.size() < b.size();
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n;
+	while (n--) {
+		cnt = st = 0;
+		cin >> s;
+
+		for (int i = 0; i < s.size(); ++i) {
+			if (isdigit(s[i])) {
+				if (cnt == 0) st = i;
+				++cnt;
+			} else {
+				if (cnt) ret.push_back(trim(s.substr(st, cnt)));
+				cnt = 0;
+			}
+		}
+		if (cnt) ret.push_back(trim(s.substr(st)));
+	}
+	sort(ret.begin(), ret.end(), compare);
+	for (string a : ret)
+		cout << a << '\n';
+}
