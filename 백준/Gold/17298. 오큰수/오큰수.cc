ef493034f299f948cbf6467babfff22f2fad886e
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-09-09 23:03:58 +0900

    [Gold IV] Title: 오큰수, Time: 176 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,31 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, t, top1;
-stack<int> s1, s2;
+int n, t;
+stack<pair<int, int>> stk;
 int ret[1000003];
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 	cin >> n;
+	fill(ret, ret + n, -1);
 	for (int i = 0; i < n; ++i) {
 		cin >> t;
-		s1.push(t);
+		while (!stk.empty() && stk.top().second < t) {
+			ret[stk.top().first] = t;
+			stk.pop();
+		}
+		stk.push({ i, t });
 	}
-
-	for (int i = n - 1; i >= 0; --i) {
-		top1 = s1.top();
-		s1.pop();
-
-		for (; !s2.empty() && top1 >= s2.top(); s2.pop());
-		if (s2.empty()) ret[i] = -1;
-		else ret[i] = s2.top();
-
-		s2.push(top1);
-	}
-
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < n; ++i)
 		cout << ret[i] << ' ';
-	}
 }
