891ace18fe0e57d2b9fe4d825027a79746ab6dcc
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-09-09 22:47:12 +0900

    [Gold IV] Title: 오큰수, Time: 200 ms, Memory: 10208 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,33 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int seq[1000001];
-int ans[1000001];
-stack<int> s;
+int n, t, top1;
+stack<int> s1, s2;
+int ret[1000003];
 
-int main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0);
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	cin >> N;
-	for (int i = 1; i <= N; ++i)
-		cin >> seq[i];
-
-	ans[N] = -1;
-	s.push(seq[N]);
-	for (int i = N - 1; i > 0; --i)
-	{
-		while (!s.empty() && seq[i] >= s.top())
-			s.pop();
-
-		if (s.empty())
-			ans[i] = -1;
-		else if (s.top() > seq[i])
-			ans[i] = s.top();
-		s.push(seq[i]);
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> t;
+		s1.push(t);
 	}
 
-	for (int i = 1; i <= N; ++i)
-		cout << ans[i] << ' ';
+	for (int i = n - 1; i >= 0; --i) {
+		top1 = s1.top();
+		s1.pop();
+
+		for (; !s2.empty() && top1 >= s2.top(); s2.pop());
+		if (s2.empty()) ret[i] = -1;
+		else ret[i] = s2.top();
+
+		s2.push(top1);
+	}
+
+	for (int i = 0; i < n; ++i) {
+		cout << ret[i] << ' ';
+	}
 }
