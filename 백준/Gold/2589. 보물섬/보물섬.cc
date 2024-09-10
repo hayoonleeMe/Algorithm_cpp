d10d0c35f84e9fdd8333f4c45945ddf2ca1ebcc4
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-09-10 14:52:28 +0900

    [Gold V] Title: 보물섬, Time: 116 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -1,58 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define X first
-#define Y second
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-int N, M;
-string board[52];
-int dist[52][52];
-vector<pair<int, int>> v;
-queue<pair<int, int>> q;
-int ans;
 
-int main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0);
+const int dy[] = { -1,0,1,0 };
+const int dx[] = { 0,1,0,-1 };
+int n, m, y, x, visited[53][53], ret;
+string board[53];
+vector<pair<int, int>> land;
 
-	cin >> N >> M;
-	for (int i = 0; i < N; ++i)
-	{
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i) {
 		cin >> board[i];
-		for (int j = 0; j < M; ++j)
-			if (board[i][j] == 'L')
-				v.push_back({ i, j });
-	}
-
-	for (const pair<int, int>& start : v)
-	{
-		for (int i = 0; i < N; ++i)
-			fill(dist[i], dist[i] + M, -1);
-
-		dist[start.X][start.Y] = 0;
-		q.push({ start.X, start.Y });
-
-		while (!q.empty())
-		{
-			pair<int, int> cur = q.front();
-			q.pop();
-			ans = max(ans, dist[cur.X][cur.Y]);
-
-			for (int dir = 0; dir < 4; ++dir)
-			{
-				int nx = cur.X + dx[dir];
-				int ny = cur.Y + dy[dir];
-
-				if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-					continue;
-				if (dist[nx][ny] != -1 || board[nx][ny] != 'L')
-					continue;
-
-				dist[nx][ny] = dist[cur.X][cur.Y] + 1;
-				q.push({ nx, ny });
-			}
+		for (int j = 0; j < m; ++j) {
+			if (board[i][j] == 'L') land.push_back({ i,j });
 		}
 	}
 
-	cout << ans;
+	for (auto L : land) {
+		memset(visited, -1, sizeof(visited));
+
+		queue<pair<int, int>> q;
+		visited[L.first][L.second] = 0;
+		q.push(L);
+
+		while (q.size()) {
+			tie(y, x) = q.front();
+			q.pop();
+			for (int dir = 0; dir < 4; ++dir) {
+				int ny = y + dy[dir];
+				int nx = x + dx[dir];
+				if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+				if (visited[ny][nx] >= 0 || board[ny][nx] != 'L') continue;
+				visited[ny][nx] = visited[y][x] + 1;
+				q.push({ ny, nx });
+				ret = max(ret, visited[ny][nx]);
+			}
+		}
+	}
+	cout << ret;
 }
