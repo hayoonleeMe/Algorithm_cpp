323af2fe2b072145790e80edb884b61a345048a7
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-09-12 13:10:30 +0900

    [Gold IV] Title: 인구 이동, Time: 176 ms, Memory: 2252 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.cc
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -1,87 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define X first
-#define Y second
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-int N, L, R;
-int board[52][52];
-bool vis[52][52];
-int ans;
 
-int main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0);
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, 1, 0, -1 };
+int n, l, r, ret;
+int board[53][53];
+bool visited[53][53];
 
-	cin >> N >> L >> R;
-	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < N; ++j)
+void dfs(int y, int x, int& sum, vector<pair<int, int>>& uni) {
+	for (int dir = 0; dir < 4; ++dir) {
+		int ny = y + dy[dir];
+		int nx = x + dx[dir];
+		if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+		if (visited[ny][nx]) continue;
+		const int gap = abs(board[y][x] - board[ny][nx]);
+		if (l <= gap && gap <= r) {
+			sum += board[ny][nx];
+			visited[ny][nx] = 1;
+			uni.push_back({ ny,nx });
+			dfs(ny, nx, sum, uni);
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n >> l >> r;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
 			cin >> board[i][j];
+		}
+	}
 
-	while (1)
-	{
-		bool transfer = false;
-		for (int i = 0; i < N; ++i)
-		{
-			for (int j = 0; j < N; ++j)
-			{
-				if (vis[i][j])
-					continue;
-
-				// 연합 생성
-				vector<pair<int, int>> v;
-
-				vis[i][j] = ans + 1;
-				v.push_back({ i, j });
-				int sum = board[i][j], cnt = 1;
-				int idx = 0;
-				while (idx < v.size())
-				{
-					pair<int, int> cur = v[idx++];
-
-					for (int dir = 0; dir < 4; ++dir)
-					{
-						int nx = cur.X + dx[dir];
-						int ny = cur.Y + dy[dir];
-
-						if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-							continue;
-
-						if (vis[nx][ny])
-							continue;
-
-						int gap = abs(board[cur.X][cur.Y] - board[nx][ny]);
-						if (gap >= L && gap <= R)
-						{
-							sum += board[nx][ny];
-							++cnt;
-
-							vis[nx][ny] = ans + 1;
-							v.push_back({ nx, ny });
-						}
-					}
-				}
-
-				// 인구 이동
-				if (cnt > 1)
-				{
-					int uni = sum / cnt;
-					for (const pair<int, int>& p : v)
-						board[p.X][p.Y] = uni;
-					transfer = true;
+	while (true) {
+		memset(visited, 0, sizeof(visited));
+		bool flag = 0;
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				if (visited[i][j]) continue;
+				vector<pair<int, int>> uni;
+				visited[i][j] = 1;
+				uni.push_back({ i,j });
+				int sum = board[i][j];
+				dfs(i, j, sum, uni);
+				if (uni.size() == 1) continue;
+				flag = 1;
+				sum /= uni.size();
+				for (auto p : uni) {
+					board[p.first][p.second] = sum;
 				}
 			}
 		}
-
-		if (transfer)
-		{
-			++ans;
-			for (int i = 0; i < N; ++i)
-				fill(vis[i], vis[i] + N, 0);
-		}
-		else
-			break;
+		if (!flag) break;
+		++ret;
 	}
-
-	cout << ans;
+	cout << ret;
 }
