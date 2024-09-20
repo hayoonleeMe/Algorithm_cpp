27d7d4dcf8e06444d329afbe779f1b69155f3d0d
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-09-20 17:06:55 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -1,58 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, ret = INT_MIN;
+int n, ret = -987654321;
 string s;
-vector<int> op_idx, pr;
-stack<char> stk_ator;
-stack<int> stk_rand;
+vector<int> num;
+vector<char> oper;
 
-bool isOp(char c) {
-	return c == '+' || c == '-' || c == '*';
+int calc(char op, int a, int b) {
+	if (op == '+') return a + b;
+	if (op == '-') return a - b;
+	if (op == '*') return a * b;
 }
 
-void consume() {
-	while (stk_ator.size()) {
-		char op = stk_ator.top(); stk_ator.pop();
-		int rand2 = stk_rand.top(); stk_rand.pop();
-		int rand1 = stk_rand.top(); stk_rand.pop();
-		if (op == '+') rand1 += rand2;
-		if (op == '-') rand1 -= rand2;
-		if (op == '*') rand1 *= rand2;
-		stk_rand.push(rand1);
-	}
-}
-
-void go() {
-	for (int i = 0; i < s.size(); ++i) {
-		if (pr[i] == 2) {
-			stk_rand.push((int)(s[i] - '0'));
-			stk_rand.push((int)(s[i + 2] - '0'));
-			stk_ator.push(s[i + 1]);
-			consume();
-			i += 2;
-			continue;
-		}
-
-		if (isOp(s[i])) stk_ator.push(s[i]);
-		else stk_rand.push((int)(s[i] - '0'));
-		if (stk_rand.size() >= 2) consume();
-	}
-	consume();
-	const int t = stk_rand.top(); stk_rand.pop();
-	ret = max(ret, t);
-}
-
-void combi(int start, int r, int cnt) {
-	if (cnt == r) {
-		go();
+void go(int here, int sum) {
+	if (here == num.size() - 1) {
+		ret = max(ret, sum);
 		return;
 	}
 
-	for (int i = start + 2; i < op_idx.size(); ++i) {
-		pr[op_idx[i] - 1] = pr[op_idx[i]] = pr[op_idx[i] + 1] = 2;
-		combi(i, r, cnt + 1);
-		pr[op_idx[i] - 1] = pr[op_idx[i]] = pr[op_idx[i] + 1] = 1;
+	go(here + 1, calc(oper[here], sum, num[here + 1]));
+	if (here + 2 < num.size()) {
+		int temp = calc(oper[here + 1], num[here + 1], num[here + 2]);
+		go(here + 2, calc(oper[here], sum, temp));
 	}
 }
 
@@ -60,14 +29,11 @@ int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 	cin >> n >> s;
-	for (int i = 0; i < s.size(); ++i) {
-		if (isOp(s[i])) op_idx.push_back(i);
-		pr.push_back(1);
+	for (int i = 0; i < n; ++i) {
+		if (i % 2 == 0) num.push_back(s[i] - '0');
+		else oper.push_back(s[i]);
 	}
 
-	const int max_pa = (op_idx.size() + 1) / 2;
-	for (int pa = 0; pa <= max_pa; ++pa) {
-		combi(-2, pa, 0);
-	}
+	go(0, num[0]);
 	cout << ret;
 }
