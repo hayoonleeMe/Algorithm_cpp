d512be3cee43be983641a3963e1077655b91c1d7
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-09-22 16:48:15 +0900

    [Gold IV] Title: 알파벳, Time: 368 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -5,8 +5,7 @@ const int dy[] = { -1, 0, 1, 0 };
 const int dx[] = { 0, 1, 0, -1 };
 int r, c, ret;
 string board[22];
-bool visited[22][22];
-map<char, int> mp;
+bool alpha[30];
 
 void go(int y, int x, int num) {
 	ret = max(ret, num);
@@ -15,13 +14,10 @@ void go(int y, int x, int num) {
 		int ny = y + dy[dir];
 		int nx = x + dx[dir];
 		if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-		if (visited[ny][nx]) continue;
-		if (mp[board[ny][nx]]) continue;
-		visited[ny][nx] = 1;
-		mp[board[ny][nx]] = 1;
+		if (alpha[board[ny][nx] - 'A']) continue;
+		alpha[board[ny][nx] - 'A'] = 1;
 		go(ny, nx, num + 1);
-		visited[ny][nx] = 0;
-		mp[board[ny][nx]] = 0;
+		alpha[board[ny][nx] - 'A'] = 0;
 	}
 }
 
@@ -32,8 +28,7 @@ int main() {
 	for (int i = 0; i < r; ++i) 
 		cin >> board[i];
 
-	mp[board[0][0]] = 1;
-	visited[0][0] = 1;
+	alpha[board[0][0] - 'A'] = 1;
 	go(0, 0, 1);
 	cout << ret;
 }
