4c24b95cf6d7077e41bf836283d0afdbdbe03162
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-09-26 02:33:29 +0900

    [Gold I] Title: 동전 뒤집기, Time: 260 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
@@ -2,38 +2,39 @@
 using namespace std;
 
 int n, ret = 1e6;
-string coin[22];
+string s;
+int coin[22];
 
-void flipRow(int r, vector<string>& b) {
-	for (int c = 0; c < n; ++c)
-		b[r][c] = b[r][c] == 'H' ? 'T' : 'H';
-}
-
-void go(vector<string>& b) {
-	int cnt = 0;
-	for (int c = 0; c < n; ++c) {
-		int t = 0;
-		for (int r = 0; r < n; ++r) {
-			if (b[r][c] == 'T') ++t;
+void go(int here) {
+	if (here == n) {
+		int cnt = 0;
+		for (int c = 0; c < n; ++c) {
+			int t = 0;
+			for (int r = 0; r < n; ++r) {
+				if (coin[r] & (1 << c)) ++t;
+			}
+			cnt += min(t, n - t);
 		}
-		if (t > n / 2) t = n - t;
-		cnt += t;
+		ret = min(ret, cnt);
+		return;
 	}
-	ret = min(ret, cnt);
+	go(here + 1);
+	coin[here] = ~coin[here];
+	go(here + 1);
 }
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	
-	cin >> n;
-	for (int i = 0; i < n; ++i)
-		cin >> coin[i];
 
-	for (int i = 0; i < (1 << n); ++i) {
-		vector<string> b(coin, coin + n);
-		for (int j = 0; j < n; ++j)
-			if (i & (1 << j)) flipRow(j, b);
-		go(b);
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> s;
+		int value = 1;
+		for (int j = 0; j < n; ++j) {
+			if (s[j] == 'T') coin[i] |= value;
+			value *= 2;
+		}
 	}
+	go(0);
 	cout << ret;
 }
