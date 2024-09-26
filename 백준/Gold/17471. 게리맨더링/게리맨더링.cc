653a84f4aff4cd7ca51f3682e73797367d682f8c
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-09-26 20:18:34 +0900

    [Gold III] Title: 게리맨더링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.cc
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.cc
@@ -1,62 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 1e6;
-int n, c, t, total, ret = INF;
-int a[12];
-int visited[12];
+const int INF = 987654321;
+int n, a[12], t, c, ret = INF;
+int visited[12], comp[12];
 vector<int> adj[12];
 
-int bfs(int team) {
-	int cnt = 0, sum = 0;
-	vector<int> tv(visited, visited + n + 1);
-	queue<int> q;
-	for (int i = 1; i <= n; ++i) {
-		if (tv[i] != team) continue;
-		++cnt;
-		tv[i] = 3 * team;
-		q.push(i);
-		while (q.size()) {
-			int cur = q.front(); q.pop();
-			sum += a[cur];
-			for (int nxt : adj[cur]) {
-				if (tv[nxt] != team) continue;
-				tv[nxt] = 3 * team;
-				q.push(nxt);
-			}
-		}
+pair<int, int> dfs(int here, int team) {
+	pair<int, int> ret = { 1, a[here] };
+	visited[here] = 1;
+	for (int nxt : adj[here]) {
+		if (visited[nxt] || comp[nxt] != team) continue;
+		pair<int, int> temp = dfs(nxt, team);
+		ret.first += temp.first;
+		ret.second += temp.second;
 	}
-	if (cnt == 1) return sum;
-	else return -1;
+	return ret;
 }
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 	cin >> n;
-	for (int i = 1; i <= n; ++i) {
+	for (int i = 1; i <= n; ++i)
 		cin >> a[i];
-		total += a[i];
-	}
 	for (int i = 1; i <= n; ++i) {
 		cin >> c;
 		for (int j = 0; j < c; ++j) {
 			cin >> t;
 			adj[i].push_back(t);
+			adj[t].push_back(i);
 		}
 	}
 
 	for (int i = 1; i < (1 << n) - 1; ++i) {
 		memset(visited, 0, sizeof(visited));
-		for (int j = 1; j <= n; ++j) {
-			if (i & (1 << j)) visited[j] = 1;
-			else visited[j] = 2;
+		memset(comp, 0, sizeof(comp));
+		int idx0 = -1, idx1 = -1;
+		for (int j = 0; j < n; ++j) {
+			if (i & (1 << j)) {
+				comp[j + 1] = 1;
+				idx1 = j + 1;
+			} else idx0 = j + 1;
 		}
-		int t1 = bfs(1);
-		int t2 = bfs(2);
-		if (t1 > 0 && t2 > 0) ret = min(ret, abs(t1 - t2));
+		pair<int, int> ret0 = dfs(idx0, 0);
+		pair<int, int> ret1 = dfs(idx1, 1);
+		if (ret0.first + ret1.first == n)
+			ret = min(ret, abs(ret0.second - ret1.second));
 	}
-
 	if (ret == INF) ret = -1;
 	cout << ret;
 }
