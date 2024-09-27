c9292775dfb4122c584dba5976f1ede92a8842d9
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-09-27 18:15:37 +0900

    [Gold IV] Title: 가르침, Time: 16 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.cc
+++ 백준/Gold/1062. 가르침/가르침.cc
@@ -2,29 +2,23 @@
 using namespace std;
 
 int n, k, ret;
-string s, word[52];
+string s;
+int word[52];
 string alpha = "bdefghjklmopqrsuvwxyz";
 
 void go(int start, int num, int b) {
 	if (num == k - 5) {
 		int cnt = 0;
 		for (int i = 0; i < n; ++i) {
-			bool flag = 1;
-			for (char c : word[i]) {
-				int idx = (int)(c - 'a');
-				if (!(b & (1 << idx))) {
-					flag = 0;
-					break;
-				}
-			}
-			if (flag) ++cnt;
+			if ((b & word[i]) == word[i])
+				++cnt;
 		}
 		ret = max(ret, cnt);
 		return;
 	}
 
 	for (int i = start + 1; i < alpha.size(); ++i) {
-		go(i, num + 1, b | (1 << (int)(alpha[i] - 'a')));
+		go(i, num + 1, b | (1 << (alpha[i] - 'a')));
 	}
 }
 
@@ -38,9 +32,10 @@ int main() {
 	}
 	for (int i = 0; i < n; ++i) {
 		cin >> s;
-		word[i] = s.substr(4, s.size() - 8);
+		for (char c : s)
+			word[i] |= (1 << (c - 'a'));
 	}
-	int b = (1 << (int)('a' - 'a')) | (1 << (int)('c' - 'a')) | (1 << (int)('i' - 'a')) | (1 << (int)('n' - 'a')) | (1 << (int)('t' - 'a'));
+	int b = (1 << ('a' - 'a')) | (1 << ('c' - 'a')) | (1 << ('i' - 'a')) | (1 << ('n' - 'a')) | (1 << ('t' - 'a'));
 	go(-1, 0, b);
 	cout << ret;
 }
