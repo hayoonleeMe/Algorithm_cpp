5964a861fe35b3a26d2278f50efe9cc4267fbe32
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-09-27 17:53:42 +0900

    [Gold IV] Title: 가르침, Time: 136 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.cc
+++ 백준/Gold/1062. 가르침/가르침.cc
@@ -2,17 +2,17 @@
 using namespace std;
 
 int n, k, ret;
-string alpha = "bdefghjklmopqrsuvwxyz";
 string s, word[52];
-bool b[26];
+string alpha = "bdefghjklmopqrsuvwxyz";
 
-void go(int start, int num) {
+void go(int start, int num, int b) {
 	if (num == k - 5) {
 		int cnt = 0;
 		for (int i = 0; i < n; ++i) {
 			bool flag = 1;
 			for (char c : word[i]) {
-				if (!b[(int)(c - 'a')]) {
+				int idx = (int)(c - 'a');
+				if (!(b & (1 << idx))) {
 					flag = 0;
 					break;
 				}
@@ -24,10 +24,7 @@ void go(int start, int num) {
 	}
 
 	for (int i = start + 1; i < alpha.size(); ++i) {
-		int idx = (int)(alpha[i] - 'a');
-		b[idx] = 1;
-		go(i, num + 1);
-		b[idx] = 0;
+		go(i, num + 1, b | (1 << (int)(alpha[i] - 'a')));
 	}
 }
 
@@ -39,11 +36,11 @@ int main() {
 		cout << 0;
 		return 0;
 	}
-	b[(int)('a' - 'a')] = b[(int)('c' - 'a')] = b[(int)('i' - 'a')] = b[(int)('n' - 'a')] = b[(int)('t' - 'a')] = 1;
 	for (int i = 0; i < n; ++i) {
 		cin >> s;
 		word[i] = s.substr(4, s.size() - 8);
 	}
-	go(-1, 0);
+	int b = (1 << (int)('a' - 'a')) | (1 << (int)('c' - 'a')) | (1 << (int)('i' - 'a')) | (1 << (int)('n' - 'a')) | (1 << (int)('t' - 'a'));
+	go(-1, 0, b);
 	cout << ret;
 }
