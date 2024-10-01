df0afb82e25dfbd28951f6eb2ba2d9c2be8f4179
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-10-01 23:51:52 +0900

    [Gold III] Title: 현욱은 괄호왕이야!!, Time: 0 ms, Memory: 3388 KB -BaekjoonHub

--- 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
@@ -1,14 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct ss {
-	char c;
-	int idx;
-};
-
 int n, ret, t;
 string s;
-stack<ss> stk;
+stack<int> stk;
 bool v[200'003];
 
 
@@ -17,36 +12,18 @@ int main() {
 
 	cin >> n >> s;
 	for (int i = 0; i < n; ++i) {
-		if (s[i] == '(') stk.push({ s[i], i });
-		else {
-			if (stk.size()) {
-				if (stk.top().c == '(') {
-					v[i] = 1;
-					v[stk.top().idx] = 1;
-					stk.pop();
-				}
-			} else {
-				continue;
-			}
+		if (s[i] == '(') stk.push(i);
+		else if (stk.size()) {
+			v[i] = v[stk.top()] = 1;
+			stk.pop();
 		}
 	}
-	bool flag = 0;
 	for (int i = 0; i < n; ++i) {
 		if (!v[i]) {
-			flag = 0;
 			t = 0;
-			continue;
-		}
-		if (flag) {
-			if (v[i]) {
-				t += 1;
-				if (t % 2 == 0) ret = max(ret, t);
-			}
 		} else {
-			if (v[i]) {
-				flag = 1;
-				t = 1;
-			}
+			t += 1;
+			if (t % 2 == 0) ret = max(ret, t);
 		}
 	}
 	cout << ret;
