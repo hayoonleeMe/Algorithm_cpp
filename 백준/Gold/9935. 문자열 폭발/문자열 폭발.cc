72cd0f4d9966835cc4d52e0dfa2f523eb8e6da04
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-10-04 20:38:47 +0900

    [Gold IV] Title: 문자열 폭발, Time: 16 ms, Memory: 6056 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -2,33 +2,19 @@
 using namespace std;
 
 string a, b, ret;
-stack<char> stk;
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 	cin >> a >> b;
 	for (char c : a) {
-		stk.push(c);
-		if (stk.size() >= b.size() && stk.top() == b[b.size() - 1]) {
-			string s = "";
-			for (int i = 0; i < b.size(); ++i) {
-				s += stk.top();
-				stk.pop();
-			}
-			reverse(s.begin(), s.end());
-			if (s != b) {
-				for (char cc : s) stk.push(cc);
-			}
+		ret += c;
+		if (ret.size() >= b.size()) {
+			int cur = ret.find(b, ret.size() - b.size());
+			if (cur != string::npos)
+				ret.erase(cur, b.size());
 		}
 	}
-	if (stk.empty()) cout << "FRULA";
-	else {
-		while (stk.size()) {
-			ret += stk.top();
-			stk.pop();
-		}
-		reverse(ret.begin(), ret.end());
-		cout << ret;
-	}
+	if (ret.empty()) cout << "FRULA";
+	else cout << ret;
 }
