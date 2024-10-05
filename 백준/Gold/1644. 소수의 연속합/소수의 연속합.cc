e03c27527d2d2269e74c940722859b07c6533448
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-10-05 16:36:48 +0900

    [Gold III] Title: 소수의 연속합, Time: 36 ms, Memory: 9128 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -1,36 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, ret;
-
-bool check(int i) {
-	if (i <= 1) return 0;
-	if (i == 2) return 1;
-	if (i % 2 == 0) return 0;
-	for (int j = 3; j * j <= i; ++j)
-		if (i % j == 0) return 0;
-	return 1;
-}
+int n, ret, st, en, sum;
+bool che[4'000'001];
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 	cin >> n;
-	int cur = 0;
-	queue<int> q;
-	for (int i = n; i >= 2; --i) {
-		if (check(i)) {
-			cur += i;
-			q.push(i);
-			if (cur > n) {
-				cur -= q.front();
-				q.pop();
-			} else if (cur == n) {
-				++ret;
-				cur -= q.front(); 
-				q.pop();
-			}
-		}
+	vector<int> v;
+	for (int i = 2; i <= n; ++i) {
+		if (che[i]) continue;
+		for (int j = 2 * i; j <= n; j += i)
+			che[j] = 1;
+	}
+	for (int i = 2; i <= n; ++i)	
+		if (!che[i]) v.push_back(i);
+
+	while (1) {
+		if (sum >= n) sum -= v[st++];
+		else if (en == v.size()) break;
+		else sum += v[en++];
+		if (sum == n) ++ret;
 	}
 	cout << ret;
 }
