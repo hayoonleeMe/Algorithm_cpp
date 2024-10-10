6528d2f049a4de3582b3b1c5ea44358c096d9537
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-10-10 16:33:42 +0900

    [Silver I] Title: 스타트와 링크, Time: 104 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,49 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
-int arr[22][22];
+int n, ret = 987654321;
+int a[23][23];
+bool t[23];	// 0 : s, 1 : l
 
-int ans = 0x7f7f7f7f7f;
-
-int main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0);
-
-	cin >> N;
-	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < N; ++j)
-			cin >> arr[i][j];
-
-	vector<int> mask;
-	mask.resize(N, 1);
-	for (int i = 0; i < N / 2; ++i)
-		mask[i] = 0;
-
-	do
-	{
-		vector<int> start, link;
-		for (int i = 0; i < mask.size(); ++i)
-		{
-			if (mask[i] == 0)
-				start.push_back(i);
-			else
-				link.push_back(i);
+void solve() {
+	vector<int> t1, t2;
+	for (int i = 1; i <= n; ++i) {
+		if (t[i]) t1.push_back(i);
+		else t2.push_back(i);
+	}
+	int s1 = 0, s2 = 0;
+	for (int i = 0; i < n / 2; ++i) {
+		for (int j = i + 1; j < n / 2; ++j) {
+			s1 += a[t1[i]][t1[j]] + a[t1[j]][t1[i]];
+			s2 += a[t2[i]][t2[j]] + a[t2[j]][t2[i]];
 		}
-		
-		int startSum = 0;
-		for (int i = 0; i < start.size(); ++i)
-			for (int j = 0; j < start.size(); ++j)
-				startSum += arr[start[i]][start[j]];
-		
-		int linkSum = 0;
-		for (int i = 0; i < link.size(); ++i)
-			for (int j = 0; j < link.size(); ++j)
-				linkSum += arr[link[i]][link[j]];
+	}
+	ret = min(ret, abs(s1 - s2));
+}
 
-		ans = min(ans, abs(startSum - linkSum));
+void combi(int depth, int size) {
+	if (size == n / 2) {
+		solve();
+		return;
+	}
+	for (int i = depth + 1; i <= n; ++i) {
+		t[i] = 1;
+		combi(i, size + 1);
+		t[i] = 0;
+	}
+}
 
-	} while (next_permutation(mask.begin(), mask.end()));
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	cout << ans;
+	cin >> n;
+	for (int i = 1; i <= n; ++i)
+		for (int j = 1; j <= n; ++j)
+			cin >> a[i][j];
+	combi(0, 0);
+	cout << ret;
 }
