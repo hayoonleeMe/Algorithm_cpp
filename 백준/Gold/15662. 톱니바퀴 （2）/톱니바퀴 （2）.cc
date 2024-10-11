a407184735a7d471504f22ee4990e0548dda5be3
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-10-11 20:15:22 +0900

    [Gold V] Title: 톱니바퀴 (2), Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
+++ 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
@@ -3,15 +3,25 @@ using namespace std;
 
 int t, k, n, d, ret;
 string s;
-vector<int> to[1003], temp[1003];
-bool visited[1003];
+vector<int> to[1003];
 
 void go(int n, int d) {
-	visited[n] = 1;
 	if (d > 0) rotate(to[n].rbegin(), to[n].rbegin() + 1, to[n].rend());
 	else rotate(to[n].begin(), to[n].begin() + 1, to[n].end());
-	if (n + 1 <= t && !visited[n + 1] && temp[n][2] != temp[n + 1][6]) go(n + 1, (d == 1 ? -1 : 1));
-	if (n - 1 >= 1 && !visited[n - 1] && temp[n][6] != temp[n - 1][2]) go(n - 1, (d == 1 ? -1 : 1));
+}
+
+int findR(int n) {
+	for (int i = n; i <= t; ++i) {
+		if (i + 1 > t || to[i][2] == to[i+1][6]) return i;
+	}
+	return n;
+}
+
+int findL(int n) {
+	for (int i = n; i >= 1; --i) {
+		if (i - 1 < 1 || to[i][6] == to[i - 1][2]) return i;
+	}
+	return n;
 }
 
 int main() {
@@ -25,11 +35,19 @@ int main() {
 	}
 	cin >> k;
 	for (int i = 0; i < k; ++i) {
-		for (int j = 1; j <= t; ++j)
-			temp[j] = to[j];
-		memset(visited, 0, sizeof(visited));
 		cin >> n >> d;
-		go(n, d);
+		int r = findR(n);
+		int l = findL(n);
+		int dir = d;
+		for (int j = n; j <= r; ++j) {
+			go(j, dir);
+			dir = dir == 1 ? -1 : 1;
+		}
+		dir = d == 1 ? -1 : 1;
+		for (int j = n - 1; j >= l; --j) {
+			go(j, dir);
+			dir = dir == 1 ? -1 : 1;
+		}
 	}
 	for (int i = 1; i <= t; ++i)
 		if (to[i][0] == 1) ++ret;
