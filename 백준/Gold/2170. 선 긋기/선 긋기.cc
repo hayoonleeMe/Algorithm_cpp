e3d2451b2d9f53a8dd5a8b3fc52cafb3f417b2bb
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-10-22 15:01:34 +0900

    [Gold V] Title: 선 긋기, Time: 340 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.cc
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -1,60 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
-pair<int, int> arr[1000'005];
-vector<pair<int, int>> v;
-long long ans;
+int n, ret, cur = -1000000003;
+pair<int, int> a[1000003];
 
-int main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0);
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	cin >> N;
-	for (int i = 0; i < N; ++i)
-		cin >> arr[i].first >> arr[i].second;
-	sort(arr, arr + N);
-
-	v.push_back({ arr[0].first, arr[0].second });
-	for (int i = 1; i < N; ++i)
-	{
-		int x = arr[i].first;
-		int y = arr[i].second;
-
-		bool processed = false;
-		for (int j = 0; j < v.size(); ++j)
-		{
-			int px = v[j].first;
-			int py = v[j].second;
-
-			// full in ... p.x - x - y - p.y
-			if (px <= x && y <= py)
-			{
-				processed = true;
-				break;
-			}
-
-			// partially in ... p.x - x - p.y - y or x - p.x - y - p.y
-			if (px <= x && x <= py)
-			{
-				v[j].second = y;
-				processed = true;
-				break;
-			}
-			else if (px <= y && y <= py)
-			{
-				v[j].first = x;
-				processed = true;
-				break;
-			}
-		}
-
-		// out p.x - p.y - x - y]
-		if (!processed)
-			v.push_back({ x, y });
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+		cin >> a[i].first >> a[i].second;
+	sort(a, a + n);
+	for (auto& p : a) {
+		cur = max(cur, p.first);
+		if (cur >= p.second) continue;
+		ret += p.second - cur;
+		cur = p.second;
 	}
-
-	for (auto p : v)
-		ans += (p.second - p.first);
-	cout << ans;
+	cout << ret;
 }
