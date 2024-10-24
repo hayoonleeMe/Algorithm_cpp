b37889ddbe3085b4a208257605148d5d9bfafc2e
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-10-24 16:59:04 +0900

    [Silver II] Title: 파닭파닭, Time: 368 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/14627. 파닭파닭/파닭파닭.cc
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.cc
@@ -1,38 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-int S, C;
-int L[1'000'002];
+ll s, c, ret = 1;
+ll v[1000003];
 
-bool Solve(long long mid)
-{
-	long long cnt = 0;
-	for (int i = 0; i < S; ++i)
-		cnt += L[i] / mid;
-	return cnt >= C;
+bool solve(ll mid) {
+	ll cnt = 0;
+	for (int i = 0; i < s; ++i)
+		cnt += v[i] / mid;
+	return cnt >= c;
 }
 
-int main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0);
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	cin >> S >> C;
-	for (int i = 0; i < S; ++i)
-		cin >> L[i];
-
-	long long st = 1;
-	long long en = 1'000'000'000;
-	while (st < en)
-	{
-		long long mid = (st + en + 1) / 2;
-		if (Solve(mid))
-			st = mid;
-		else
-			en = mid - 1;
+	cin >> s >> c;
+	ll r = -1;
+	for (int i = 0; i < s; ++i) {
+		cin >> v[i];
+		r = max(r, v[i]);
 	}
-
-	long long remain = -st * C;
-	for (int i = 0; i < S; ++i)
-		remain += L[i];
-	cout << remain;
+	ll l = 1;
+	while (l <= r) {
+		ll mid = (l + r) / 2;
+		if (solve(mid)) {
+			ret = mid;
+			l = mid + 1;
+		} else r = mid - 1;
+	}
+	int cnt = 0;
+	for (int i = 0; i < s; ++i) {
+		while (cnt < c && v[i] >= ret) {
+			v[i] -= ret;
+			++cnt;
+		}
+	}
+	cout << accumulate<ll*, ll>(v, v + s, 0);
 }
