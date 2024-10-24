1ab9fd634e07e381fbbd885cc551f50f40e7ac64
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-10-24 16:30:20 +0900

    [Silver IV] Title: 암기왕, Time: 564 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.cc
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -1,27 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int T, N, M;
-int a[1'000'002];
-int b[1'000'002];
+int t, n, m;
+int a[1000003], b[1000003];
 
-int main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0);
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	cin >> T;
-	while (T--)
-	{
-		cin >> N;
-		for (int i = 0; i < N; ++i)
+	cin >> t;
+	while (t--) {
+		cin >> n;
+		for (int i = 0; i < n; ++i)
 			cin >> a[i];
-		sort(a, a + N);
-
-		cin >> M;
-		for (int i = 0; i < M; ++i)
+		cin >> m;
+		for (int i = 0; i < m; ++i)
 			cin >> b[i];
-
-		for (int i = 0; i < M; ++i)
-			cout << binary_search(a, a + N, b[i]) << '\n';
+		sort(a, a + n);
+		for (int i = 0; i < m; ++i)
+			cout << binary_search(a, a + n, b[i]) << '\n';
 	}
 }
