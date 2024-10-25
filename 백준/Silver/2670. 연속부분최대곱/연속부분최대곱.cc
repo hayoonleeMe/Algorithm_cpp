502bbcf1cd6a39db36863de2e39786d1b6790c94
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-10-25 20:29:45 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
@@ -5,11 +5,13 @@ int n;
 double v[10003], dp[10003];
 
 int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
 	cin >> n;
 	for (int i = 1; i <= n; ++i)
 		cin >> v[i];
 	for (int i = 1; i <= n; ++i) {
 		dp[i] = max(v[i], dp[i - 1] * v[i]);
 	}
-	printf("%0.3lf", *max_element(dp, dp + n + 1));
+	cout << fixed << setprecision(3) << *max_element(dp, dp + n + 1);
 }
