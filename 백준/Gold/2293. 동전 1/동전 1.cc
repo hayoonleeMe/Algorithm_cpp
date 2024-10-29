44b4064eb8ac35a6d1452338a99f093f2af0e6d5
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-10-29 16:10:54 +0900

    [Gold IV] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -1,22 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, K;
-int a[105];
-int d[10005];
+int n, k, t;
+int dp[10003];
 
-int main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0);
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	cin >> N >> K;
-	for (int i = 0; i < N; ++i)
-		cin >> a[i];
-
-	d[0] = 1;
-	for (int i = 0; i < N; ++i)
-		for (int j = a[i]; j <= K; ++j)
-			d[j] += d[j - a[i]];
-
-	cout << d[K];
+	cin >> n >> k;
+	dp[0] = 1;
+	for (int i = 0; i < n; ++i) {
+		cin >> t;
+		for (int j = t; j <= k; ++j)
+			dp[j] += dp[j - t];
+	}
+	cout << dp[k];
 }
