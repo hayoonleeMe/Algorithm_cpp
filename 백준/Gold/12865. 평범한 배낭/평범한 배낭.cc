44805cf5fa47103fa512a4197eec4affee66d536
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-10-29 17:33:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,26 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, K;
-int W[105];
-int V[105];
-int d[100005];
+int n, k, w, v;
+int dp[100003];
 
-int main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0);
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	cin >> N >> K;
-	for (int i = 0; i < N; ++i)
-		cin >> W[i] >> V[i];
-
-	for (int i = 0; i < N; ++i)
-	{
-		for (int j = K; j > 0; --j)
-		{
-			if (j >= W[i])
-				d[j] = max(d[j], d[j - W[i]] + V[i]);
+	cin >> n >> k;
+	for (int i = 0; i < n; ++i) {
+		cin >> w >> v;
+		for (int j = k; j >= w; --j) {
+			dp[j] = max(dp[j], dp[j - w] + v);
 		}
 	}
-	cout << *max_element(d, d + K + 1);
+	cout << dp[k];
 }
