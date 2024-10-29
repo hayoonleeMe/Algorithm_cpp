94984e745e46173ebd2c349d818746dcdfcf096d
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2024-10-29 16:18:02 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,29 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, K;
-int a[105];
-int d[10005];
+const int INF = 987654321;
+int n, k, t;
+int dp[10003];
 
-int main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0);
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	cin >> N >> K;
-	for (int i = 0; i < N; ++i)
-		cin >> a[i];
-
-	sort(a, a + N);
-
-	for (int i = 1; i <= K; ++i)
-	{
-		d[i] = 100001;
-		for (int j = 0; j < N && a[j] <= i; ++j)
-			d[i] = min(d[i], 1 + d[i - a[j]]);
+	cin >> n >> k;
+	fill(dp, dp + k + 1, INF);
+	dp[0] = 0;
+	for (int i = 0; i < n; ++i) {
+		cin >> t;
+		for (int j = t; j <= k; ++j)
+			dp[j] = min(dp[j], dp[j - t] + 1);
 	}
-
-	if (d[K] > 100000)
-		cout << -1;
-	else
-		cout << d[K];
+	if (dp[k] == INF) dp[k] = -1;
+	cout << dp[k];
 }
